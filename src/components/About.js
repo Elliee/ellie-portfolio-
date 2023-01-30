57cc15bf8d59324a16e2cfdcdcdef5e378826010
Ellie Stevens <elliebstevens@gmail.com>
2023-01-30 21:19:39 +1100

    fix layout

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
           <p>I recently decided to change careers into Software Development after teaching myself to code during the Covid lockdowns. Prior to this, I was working in tech within Sales/Customer success roles and was always fascinated learning about what the engineers did and the different projects they worked on.</p>
           <p>I completed a Web Development bootcamp in 2022 where I learned technical skills such as Ruby on Rails, SQL, HTML, CSS, Javascript, and Bootstrap while building applications for the web. I thoroughly enjoyed the challenge of learning complex topics and being able to apply these skills to build projects with team members.</p>
           <p>I am a naturally curious person and love to learn new things, and am excited to grow my skills and continue my career journey in Software Development.</p>
-          <p>Outside the world of coding I enjoy staying active by riding my bike and practicing yoga, travel, reading, cooking and good coffee.</p>
+          <p className="margin__btm">Outside the world of coding I enjoy staying active by riding my bike and practicing yoga, travel, reading, cooking and good coffee.</p>
           <a href="resume" className="resume__btn">Resume</a>
         </div>
         <div>
